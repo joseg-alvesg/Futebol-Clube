f705677999c1757c5428172b7344e1270c14d620
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-14 00:26:56 -0300

    fix

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -1,18 +1,20 @@
-import * as sinon from 'sinon';
-import * as chai from 'chai';
+import * as sinon from "sinon";
+import * as chai from "chai";
 // @ts-ignore
-import chaiHttp = require('chai-http');
+import chaiHttp = require("chai-http");
 
-import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import { App, app } from "../app";
+import Example from "../database/models/ExampleModel";
 
-import { Response } from 'superagent';
+import { Response } from "superagent";
+import { start } from "repl";
+import { request } from "http";
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+describe("Seu teste", () => {
   /**
    * Exemplo do uso de stubs com tipos
    */
@@ -39,27 +41,20 @@ describe('Seu teste', () => {
   //   expect(...)
   // });
 
-  it('integration of route teams', async () => {
-    const chaiHttpResponse = await chai
-      .request(app)
-      .get('/teams');
-    
+  it("integration of route teams", async () => {
+    const chaiHttpResponse = await chai.request(app).get("/teams");
+
     expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.an('array');
+    expect(chaiHttpResponse.body).to.be.an("array");
 
-    const chaiHttpResponse2 = await chai
-      .request(app)
-      .get('/teams/1');
-    
+    const chaiHttpResponse2 = await chai.request(app).get("/teams/1");
+
     expect(chaiHttpResponse2.status).to.be.equal(200);
-    expect(chaiHttpResponse2.body).to.be.an('object');
+    expect(chaiHttpResponse2.body).to.be.an("object");
 
-    const chaiHttpResponse3 = await chai
-      .request(app)
-      .get('/teams/999');
+    const chaiHttpResponse3 = await chai.request(app).get("/teams/999");
 
     expect(chaiHttpResponse3.status).to.be.equal(404);
-    expect(chaiHttpResponse3.body).to.deep.equal({message: 'Team not found'});
+    expect(chaiHttpResponse3.body).to.deep.equal({ message: "Team not found" });
   });
-
 });
