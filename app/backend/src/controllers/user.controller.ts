9e6798e57451e8c8a747137c23761e52a4bb46c3
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-14 16:31:18 -0300

    insert: mapStatusHTTP

--- app/backend/src/controllers/user.controller.ts
+++ app/backend/src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
 import UserService from '../services/user.service';
 
 export default class UserController {
@@ -7,6 +8,6 @@ export default class UserController {
   public login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const response = await this.userService.loginEmail({ email, password });
-    return res.status(200).json({ token: response.data });
+    return res.status(mapStatusHTTP(response.status)).json({ token: response.data });
   };
 }
