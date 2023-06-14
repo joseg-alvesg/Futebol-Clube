cfd3681c90ad4e5efabcda2a6d51b5c5d92dd1f6
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-14 17:46:32 -0300

    fix: returns

--- app/backend/src/routes/user.router.ts
+++ app/backend/src/routes/user.router.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import validateLogin from '../middlewares/user.validation';
 import UserController from '../controllers/user.controller';
 
 const userController = new UserController();
 
 const router = Router();
 
-router.post('/', userController.login);
+router.post(
+  '/',
+  validateLogin,
+  userController.login,
+);
 
 export default router;
--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -4,22 +4,28 @@ import UserModel from '../models/user.model';
 import { ServiceResponse } from '../utils/serviceResponse';
 import Token from '../utils/token';
 
+type tokenModel = { token: string };
+
 export default class UserService {
-  constructor(
-    private userModel: UserModel = new UserModel(),
-  ) {}
+  constructor(private userModel: UserModel = new UserModel()) {}
 
   public async loginEmail({
     email,
     password,
-  }: LoginInputs): Promise<ServiceResponse<string>> {
+  }: LoginInputs): Promise<ServiceResponse<tokenModel>> {
     const user = await this.userModel.loginEmail(email);
-    if (!user) return { status: 'NOT_FOUND', data: { message: 'User not found' } };
+    if (!user) {
+      return {
+        status: 'UNAUTHORIZED', data: { message: 'Invalid email or password' },
+      };
+    }
     const isPasswordMatching = bcrypt.compareSync(password, user.password);
-    if (!isPasswordMatching) return { status: 'UNAUTHORIZED', data: { message: 'Wrong password' } };
-
+    if (!isPasswordMatching) {
+      return {
+        status: 'UNAUTHORIZED', data: { message: 'Invalid email or password' },
+      };
+    }
     const token = Token.sign({ email });
-
-    return { status: 'SUCCESSFUL', data: token };
+    return { status: 'SUCCESSFUL', data: { token } };
   }
 }
