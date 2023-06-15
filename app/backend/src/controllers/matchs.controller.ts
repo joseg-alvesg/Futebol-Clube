aa535c4bd4e51a72e8e5ba59ad2f4a2745b49cb8
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-15 18:45:09 -0300

    implements: finish and updateMatches

--- app/backend/src/controllers/matchs.controller.ts
+++ app/backend/src/controllers/matchs.controller.ts
@@ -7,8 +7,22 @@ export default class MatchController {
 
   public getAll = async (req: Request, res: Response) => {
     const progress = req.query.inProgress;
-    const matches = await this.matchService.getAll(progress);
+    const matches = await this.matchService.getAll(progress as string);
     const { status, data } = matches;
+    return res.status(mapStatusHTTP(status)).json(data);
+  };
+
+  public finishMatch = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const match = await this.matchService.finishMatch(Number(id));
+    const { status, data } = match;
+    return res.status(mapStatusHTTP(status)).json(data);
+  };
+
+  public updateGoals = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const match = await this.matchService.updateGoals(Number(id), req.body);
+    const { status, data } = match;
     return res.status(mapStatusHTTP(status)).json(data);
   };
 }
