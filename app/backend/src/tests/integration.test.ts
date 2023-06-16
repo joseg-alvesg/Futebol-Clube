b67e20ab8c0533e2a9741564f4a653f4ca0a3a22
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-15 21:54:58 -0300

    coverage

--- app/backend/src/tests/integration.test.ts
+++ app/backend/src/tests/integration.test.ts
@@ -162,4 +162,107 @@ describe("Seu teste", () => {
       "Token must be a valid token"
     );
   });
+
+  it("integration of route matches", async function () {
+    const chaiHttpResponse = await chai.request(app).post("/login").send({
+      email: "admin@admin.com",
+      password: "secret_admin",
+    });
+
+    const chaiHttpResponse2 = await chai
+      .request(app)
+      .get("/matches")
+      .set("Authorization", `${chaiHttpResponse.body.token}`)
+      .send();
+
+    expect(chaiHttpResponse2.status).to.be.equal(200);
+    expect(chaiHttpResponse2.body).to.be.an("array");
+    
+    const chaiHttpResponse3 = await chai
+      .request(app)
+      .patch("/matches/1")
+      .set("Authorization", `${chaiHttpResponse.body.token}`)
+      .send({
+        homeTeamGoals: 1,
+        awayTeamGoals: 1,
+      });
+
+    expect(chaiHttpResponse3.status).to.be.equal(200);
+    expect(chaiHttpResponse3.body).to.be.an("object");
+    expect(chaiHttpResponse3.body).to.have.property("message");
+    expect(chaiHttpResponse3.body.message).to.be.equal("Goals updated");
+
+
+    const chaiHttpResponse4 = await chai
+      .request(app)
+      .patch("/matches/1/finish")
+      .set("Authorization", `${chaiHttpResponse.body.token}`)
+      .send();
+
+    expect(chaiHttpResponse4.status).to.be.equal(200);
+    expect(chaiHttpResponse4.body).to.be.an("object");
+    expect(chaiHttpResponse4.body).to.have.property("message");
+    expect(chaiHttpResponse4.body.message).to.be.equal("Finished");
+      
+  const chaiHttpResponse5 = await chai
+      .request(app)
+      .post("/matches")
+      .set("Authorization", `${chaiHttpResponse.body.token}`)
+      .send({
+        homeTeamId: 1,
+        awayTeamId: 2,
+        homeTeamGoals: 0,
+        awayTeamGoals: 0,
+      });
+
+    expect(chaiHttpResponse5.status).to.be.equal(201);
+    expect(chaiHttpResponse5.body).to.be.an("object");
+    expect(chaiHttpResponse5.body).to.have.property("id");
+    expect(chaiHttpResponse5.body).to.have.property("homeTeamId");
+    expect(chaiHttpResponse5.body).to.have.property("awayTeamId");
+    expect(chaiHttpResponse5.body).to.have.property("homeTeamGoals");
+    expect(chaiHttpResponse5.body).to.have.property("awayTeamGoals");
+    expect(chaiHttpResponse5.body).to.have.property("inProgress");
+
+    const chaiHttpResponse6 = await chai
+      .request(app)
+      .post("/matches")
+      .set("Authorization", `${chaiHttpResponse.body.token}`)
+      .send({
+        homeTeamId: 1,
+        awayTeamId: 1,
+        homeTeamGoals: 0,
+        awayTeamGoals: 0,
+      });
+
+    expect(chaiHttpResponse6.status).to.be.equal(422);
+    expect(chaiHttpResponse6.body).to.be.an("object");
+    expect(chaiHttpResponse6.body).to.have.property("message");
+    expect(chaiHttpResponse6.body.message).to.be.equal("It is not possible to create a match with two equal teams");
+
+    const chaiHttpResponse7 = await chai
+      .request(app)
+      .post("/matches")
+      .set("Authorization", `${chaiHttpResponse.body.token}`)
+      .send({
+        homeTeamId: 1,
+        awayTeamId: 999,
+        homeTeamGoals: 0,
+        awayTeamGoals: 0,
+      });
+
+    expect(chaiHttpResponse7.status).to.be.equal(404);
+    expect(chaiHttpResponse7.body).to.be.an("object");
+    expect(chaiHttpResponse7.body).to.have.property("message");
+    expect(chaiHttpResponse7.body.message).to.be.equal("There is no team with such id!");
+
+    const chaiHttpResponse8 = await chai
+      .request(app)
+      .get("/matches")
+      .query({ inProgress: true })
+      .send();
+
+    expect(chaiHttpResponse8.status).to.be.equal(200);
+    expect(chaiHttpResponse8.body).to.be.an("array");
+  })
 });
