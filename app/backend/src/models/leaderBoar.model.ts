77bbd2d8db2be665a5a404bb5be2b8527c6b8e15
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-17 14:26:36 -0300

    implements: working model

--- app/backend/src/models/leaderBoar.model.ts
+++ app/backend/src/models/leaderBoar.model.ts
@@ -17,32 +17,87 @@ import MatchModelInit from '../database/models/MatchModelInit';
 //
 
 const query = `
-    SELECT t.team_name AS name, SUM(CASE WHEN m.home_team_goals > m.away_team_goals 
-THEN 3 WHEN m.home_team_goals = m.away_team_goals THEN 1 ELSE 0 END) AS totalPoints,
-    COUNT(*) AS totalGames,
-    SUM(CASE WHEN m.home_team_goals > m.away_team_goals THEN 1 ELSE 0 END) AS totalVictories,
-    SUM(CASE WHEN m.home_team_goals = m.away_team_goals THEN 1 ELSE 0 END) AS totalDraws,
-    SUM(CASE WHEN m.home_team_goals < m.away_team_goals THEN 1 ELSE 0 END) AS totalLosses,
-    SUM(m.home_team_goals) AS goalsFavor, SUM(m.away_team_goals) AS goalsOwn,
-    SUM(m.home_team_goals) - SUM(m.away_team_goals) AS goalsBalance,
-    ROUND((SUM(CASE WHEN m.home_team_goals > m.away_team_goals THEN 3 
-    WHEN m.home_team_goals = m.away_team_goals THEN 1 
-    ELSE 0 END) / (COUNT(*) * 3)) * 100, 2) AS efficiency
-    FROM TRYBE_FUTEBOL_CLUBE.matches AS m
-    INNER JOIN TRYBE_FUTEBOL_CLUBE.teams AS t ON m.home_team_id = t.id
-    WHERE in_progress = 1 GROUP BY team_name ORDER BY totalPoints DESC,
-    totalVictories DESC, goalsBalance DESC, goalsFavor DESC;
-  `;
+SELECT
+  t.team_name AS name,
+  SUM(
+    CASE
+      WHEN m.home_team_goals > m.away_team_goals THEN 3
+      WHEN m.home_team_goals = m.away_team_goals THEN 1
+      ELSE 0
+    END
+  ) AS totalPoints,
+  COUNT(*) AS totalGames,
+  SUM(
+    CASE
+      WHEN m.home_team_goals > m.away_team_goals THEN 1
+      ELSE 0
+    END
+  ) AS totalVictories,
+  SUM(
+    CASE
+      WHEN m.home_team_goals = m.away_team_goals THEN 1
+      ELSE 0
+    END
+  ) AS totalDraws,
+  SUM(
+    CASE
+      WHEN m.home_team_goals < m.away_team_goals THEN 1
+      ELSE 0
+    END
+  ) AS totalLosses,
+  SUM(m.home_team_goals) AS goalsFavor,
+  SUM(m.away_team_goals) AS goalsOwn,
+  SUM(m.home_team_goals) - SUM(m.away_team_goals) AS goalsBalance,
+  ROUND(
+    (
+      SUM(
+        CASE
+          WHEN m.home_team_goals > m.away_team_goals THEN 3
+          WHEN m.home_team_goals = m.away_team_goals THEN 1
+          ELSE 0
+        END
+      ) / (COUNT(*) * 3)
+    ) * 100,
+    2
+  ) AS efficiency
+FROM
+  TRYBE_FUTEBOL_CLUBE.matches as m
+  INNER JOIN TRYBE_FUTEBOL_CLUBE.teams as t ON m.home_team_id = t.id
+WHERE
+  m.in_progress = false
+GROUP BY
+  team_name
+ORDER BY
+  totalPoints DESC,
+  totalVictories DESC,
+  goalsBalance DESC,
+  goalsFavor DESC;
+`;
 
 export default class LeaderBoardModel {
   private matchModel = MatchModelInit;
 
+  // private static convertToLeaderBoardInterface(
+  //   leaderBoard: any[],
+  // ) {
+  //   return leaderBoard.map((team) => ({
+  //     name: team.name,
+  //     totalPoints: team.totalPoints,
+  //     totalGames: team.totalGames,
+  //     totalVictories: team.totalVictories,
+  //     totalDraws: team.totalDraws,
+  //     totalLosses: team.totalLosses,
+  //     goalsFavor: team.goalsFavor,
+  //     goalsOwn: team.goalsOwn,
+  //     goalsBalance: team.goalsBalance,
+  //     efficiency: team.efficiency,
+  //   }));
+  // }
+
   public async getLeaderBoard() {
-    const results = await this.matchModel.sequelize?.query(query);
-    // if (results) {
-    //   const leaderBoard: LeaderBoardInterface[] = results[0] as LeaderBoardInterface[];
-    //   return leaderBoard;
-    // }
-    return results?.[0]?.flat() ?? [];
+    const leaderBoard = await this.matchModel.sequelize?.query(query, {
+      type: 'SELECT',
+    });
+    return leaderBoard as unknown as LeaderBoardInterface[];
   }
 }
