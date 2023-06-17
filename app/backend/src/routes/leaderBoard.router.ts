1ccfdb017a0699415bd854c91795030ea4645d5f
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-16 23:11:17 -0300

    dont forget the bind

--- app/backend/src/routes/leaderBoard.router.ts
+++ app/backend/src/routes/leaderBoard.router.ts
@@ -4,6 +4,6 @@ import LeaderBoardController from '../controllers/leaderBoard.controller';
 const router = Router();
 const leaderBoard = new LeaderBoardController();
 
-router.get('/home', leaderBoard.getLeaderBoard);
+router.get('/home', leaderBoard.getLeaderBoard.bind(leaderBoard));
 
 export default router;
